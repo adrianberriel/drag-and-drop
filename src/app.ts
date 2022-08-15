7ca812ad46b0b013ed63e6a2eeb29e738109b10c
Adrian Berriel <berriel@gmail.com>
2022-08-15 13:24:00 -0300

    Added decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -27,8 +40,9 @@ class ProjectInput {
         console.log(this.titleInputElement.value);
     }
 
+    @autobind
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
